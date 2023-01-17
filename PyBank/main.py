627b7a2497c41034879abf64afaf59f18eb60dca
djthapa22 <djthapa@DJs-MacBook-Pro.local>
2023-01-16 19:10:48 -0600

    final Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,16 @@
+# adding import os and csv
 import os
 import csv
 
 
-# The total number of months included in the dataset- Done
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire periodo
-
 # Setting up lists to store info
-profit= []
 month_count = []
 change_M = []
 #  Addign in starting variables
 Totalp = 0
 count = 0
 profit_change = 0
+currentp = 0
 start_p = 0
 
 # Tell python to look at the right file path to find budget file
@@ -30,41 +24,46 @@ with open(budget_csv) as csv_file:
 
     # Running the loop to find values
     for row in csv_reader:
-        # Setting the months list
-        month_count.append(row[0])
-        # style two of month count
-        count+= 1
-    #   Adding in Profit values to list
-        profit.append(row[1])
-        Totalp += int(row[1])
-       
-        #  calcing average values
-       
-        final_p = int(row[1])
-        monthly_change_p = final_p - start_p
+        
+        # count of months; style 2
+        count += 1
+        
+        # Setting up the total profit for the period
+        currentp = int(row[1])
+        Totalp+= currentp
+        
+        # starting if statement to set value of profit for starting to be same as current
+        if(count == 1):
+            start_p = currentp
+            continue
+        # calcing change in profit, along with adding to values to lists (month count), (profit change),
+        else:
+            profit_change = currentp - start_p
+            month_count.append(row[0])
+            change_M.append(profit_change)
+        #    set the loop so that starting p is set to current p
+            start_p = currentp
 
+    # Calc the average profit change 
+    Sum_p = sum(change_M)
+    average_p = round((Sum_p)/(len(month_count)),2)
 
-        change_M.append(monthly_change_p)
+    # calc th value for highest and lowest change
+    greatest_inc = max(change_M)
+    greatest_dec = min(change_M)
+    #  find the index value of highest and lowest
+    inc_date = month_count[change_M.index(greatest_inc)]
+    dec_date = month_count[change_M.index(greatest_dec)]
 
-        profit_change += monthly_change_p
-
-        average_change = (profit_change/ count)
-        start_p = final_p
     
-        # calculating the values and date to greatest and least values
-        greatest_inc = max(change_M)
-        greatest_dec = min(change_M)
 
-        inc_date = month_count[change_M.index(greatest_inc)]
-        dec_date = month_count[change_M.index(greatest_dec)]
-
-    
+    #  set a unique method for printing so that line breaks look cleaner
     Output = (
     "\nFinancial Analysis\n"
    "----------------------------------\n"
-   f'Total Months: {len(month_count)}\n'
+   f'Total Months: {len(month_count)+1}\n'
     f'Total Profits: ${str(Totalp)}\n'
-     f'Average Change: $ {round(average_change,2)}\n'
+     f'Average Change: $ {average_p}\n'
     f'Greatest Increase in Profits: {str(inc_date)} ($ {str(greatest_inc)})\n'
     f'Greatest Decrease in Profits: {str(dec_date)} ($ {str(greatest_dec)})\n'
     )
